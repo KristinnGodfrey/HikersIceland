1aaa652c82938fbc2c7681963e60955f93e1817e
Fridrikhr <Fridrikhr96@gmail.com>
2020-11-01 21:02:39 +0000

    comment kominn fyrir Profile

--- src/main/java/is/hi/g/hikersicelands/hikersicelands/Controllers/HomeController.java
+++ src/main/java/is/hi/g/hikersicelands/hikersicelands/Controllers/HomeController.java
@@ -1,4 +1,4 @@
-package is.hi.g.hikersicelands.hikersicelands;
+package is.hi.g.hikersicelands.hikersicelands.Controllers;
 
 import is.hi.g.hikersicelands.hikersicelands.Entities.Hike;
 import is.hi.g.hikersicelands.hikersicelands.Services.HikeService;
