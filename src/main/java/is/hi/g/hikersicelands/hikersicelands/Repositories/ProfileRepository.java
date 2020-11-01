fe11c596c11ab621c154e5d60e167ccb97294453
Almar10 <alt15@hi.is>
2020-11-01 23:59:14 +0000

    123

--- src/main/java/is/hi/g/hikersicelands/hikersicelands/Repositories/ProfileRepository.java
+++ src/main/java/is/hi/g/hikersicelands/hikersicelands/Repositories/ProfileRepository.java
@@ -7,9 +7,12 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface ProfileRepository extends JpaRepository<Profile, Long>{
+
     Profile loginProfile(String username, String password);
     Profile createProfile(String username, String password);
     Profile privateMode(boolean priv);
 
     List<Profile> searchProfile(String name);
+
+
 }
