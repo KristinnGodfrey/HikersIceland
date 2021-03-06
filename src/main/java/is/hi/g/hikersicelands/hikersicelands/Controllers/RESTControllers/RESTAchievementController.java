dbb88ad5b67c2bdb461bbdb9777a74cec47aa792
bjorgvinhall <43784017+bjorgvinhall@users.noreply.github.com>
2021-03-06 20:16:39 +0000

    REST controllers for Achievements and Reviews

---
+++ src/main/java/is/hi/g/hikersicelands/hikersicelands/Controllers/RESTControllers/RESTAchievementController.java
@@ -0,0 +1,85 @@
+package is.hi.g.hikersicelands.hikersicelands.Controllers.RESTControllers;
+
+import is.hi.g.hikersicelands.hikersicelands.Entities.Achievement;
+import is.hi.g.hikersicelands.hikersicelands.Entities.Hike;
+import is.hi.g.hikersicelands.hikersicelands.Entities.Item;
+import is.hi.g.hikersicelands.hikersicelands.Entities.Profile;
+import is.hi.g.hikersicelands.hikersicelands.Services.AchievementService;
+import is.hi.g.hikersicelands.hikersicelands.Services.HikeService;
+import is.hi.g.hikersicelands.hikersicelands.Services.ProfileService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+public class RESTAchievementController {
+
+    private HikeService hikeService;
+    private AchievementService achievementService;
+    private ProfileService profileService;
+
+    @Autowired
+    public RESTAchievementController(HikeService hikeService, AchievementService achievementService, ProfileService profileService){
+        this.hikeService = hikeService;
+        this.achievementService = achievementService;
+        this.profileService = profileService;
+    }
+
+    @RequestMapping(value = "/rest/hikes/{hikeid}/achievements/{id}", method = RequestMethod.GET)
+    public ResponseEntity<Object> getAchievement(@PathVariable("id") long id, Model model, HttpSession httpSession){
+        Achievement achievement;
+        try {
+            achievement = achievementService.findAchievementById(id);
+        } finally { }
+
+        if (achievement == null) {
+            return ResponseEntity.notFound().build();
+        } else {
+            return ResponseEntity.ok(achievement);
+        }
+    }
+
+    @PostMapping(value ="rest/hikes/{hikeid}/achievements/{achievementid}",
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    public Object completeAchievement(@PathVariable("hikeid") long hikeid, @PathVariable("achievementid") long id, HttpSession httpSession){
+        String sessionUsername = (String) httpSession.getAttribute("username");
+        if (sessionUsername == null) {
+            return new ResponseEntity<String>(HttpStatus.UNAUTHORIZED);
+        }
+
+        Achievement achievement;
+        Hike hike;
+        try {
+            hike = hikeService.findById(hikeid).orElse(null);
+            achievement = achievementService.findAchievementById(id);
+        } finally { }
+        if (hike == null || achievement == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        // Find profile
+        Profile profile = profileService.searchProfileByUsername(sessionUsername);
+        List<Achievement> completedAchievements = profile.getCompletedAchievements();
+
+        // Add or remove achievement
+        if (completedAchievements.contains(achievement)) {
+            completedAchievements.remove(achievement);
+        } else {
+            completedAchievements.add(achievement);
+        }
+
+        // delete old and return new
+        profileService.deleteProfileByUsername(sessionUsername);
+        Profile newProfile = profileService.saveProfile(new Profile(profile.getUsername(), profile.getPassword(), profile.getName(), profile.getAge(), profile.getAdmin(), completedAchievements));
+        return ResponseEntity.ok(newProfile);
+    }
+
+}
---
+++ src/main/java/is/hi/g/hikersicelands/hikersicelands/Controllers/RESTControllers/RESTReviewController.java
@@ -0,0 +1,84 @@
+package is.hi.g.hikersicelands.hikersicelands.Controllers.RESTControllers;
+
+import is.hi.g.hikersicelands.hikersicelands.Entities.Hike;
+import is.hi.g.hikersicelands.hikersicelands.Entities.Profile;
+import is.hi.g.hikersicelands.hikersicelands.Entities.Review;
+import is.hi.g.hikersicelands.hikersicelands.Services.HikeService;
+import is.hi.g.hikersicelands.hikersicelands.Services.ProfileService;
+import is.hi.g.hikersicelands.hikersicelands.Services.ReviewService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
+import java.util.List;
+
+
+@RestController
+public class RESTReviewController {
+
+    private ReviewService reviewService;
+    private HikeService hikeService;
+    private ProfileService profileService;
+
+    @Autowired
+    public RESTReviewController(ReviewService reviewService, HikeService hikeService, ProfileService profileService){
+        this.reviewService = reviewService;
+        this.hikeService = hikeService;
+        this.profileService = profileService;
+    }
+
+    //Eyðir ákveðnu review
+    @DeleteMapping(value ="rest/hikes/{hikeid}/reviews/{reviewid}")
+    public ResponseEntity<Object> deleteReview(@PathVariable("hikeid") long hikeid, @PathVariable("reviewid") long id, HttpSession httpSession){
+        Hike hike;
+        Review review;
+        try {
+            hike = hikeService.findById(hikeid).orElse(null);
+            review = reviewService.findById(id).orElse(null);
+        } finally { }
+        if (hike == null || review == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        reviewService.deleteReviewById(id);
+        hike = hikeService.findById(hikeid).orElse(null);
+        return ResponseEntity.ok(hike);
+    }
+
+    //birtir öll review
+    @RequestMapping(value = "rest/hikes/{id}/reviews", method = RequestMethod.GET)
+    public ResponseEntity<Object> getReviews(@PathVariable("id") long id, Model model, HttpSession httpSession){
+        Hike hike;
+        try {
+            hike = hikeService.findById(id).orElse(null);
+        } finally { }
+        if (hike == null) {
+            return ResponseEntity.notFound().build();
+        }
+        List<Review> reviews = hike.getReviews();
+        return ResponseEntity.ok(reviews);
+    }
+
+    //Fall sem býr til review á ákveðið hike
+    @PostMapping(
+            value="rest/hikes/{id}/reviews",
+            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    public ResponseEntity<Object> postReviews(@PathVariable("id") long id, @RequestBody Review review, HttpSession httpSession){
+        Hike hike;
+        try {
+            hike = hikeService.findById(id).orElse(null);
+        } finally { }
+        if (hike == null) {
+            return ResponseEntity.notFound().build();
+        }
+        Review saveReview = new Review(review.getReviewText(), review.getRating(), hike);
+        reviewService.save(saveReview);
+        Hike updatedHike = hikeService.findById(id).orElseThrow(()->new IllegalArgumentException("Invalid Hike Id"));
+        return ResponseEntity.ok(updatedHike);
+    }
+
+}
